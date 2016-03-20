ef2769f602f7a29b385df9905e78e8827da11f1a
Divye Kapoor <divyekapoor@gmail.com>
2016-03-20 15:02:51 -0700

    Take a file on the command line directly.

--- csv_to_json.rs
+++ csv_to_json.rs
@@ -3,20 +3,31 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::env;
 use std::process::exit;
+use std::fs::File;
 
 fn usage() {
   println!("Usage: cat input.csv | csv_to_json");
 }
 
 fn main() {
+  let stdin = io::stdin();
+  let mut filereader;
+  let mut reader = &mut stdin.lock() as &mut BufRead;
   let args : Vec<String> = env::args().collect();
 
-  if args.len() > 1 {
+  if args.len() > 2 {
     usage();
     exit(1);
+  } else if args.len() == 2 {
+    let file = File::open(args[1].clone());
+    if !file.is_ok() {
+      usage();
+      exit(1);
+    }
+    filereader = BufReader::new(file.unwrap());
+    reader = &mut filereader;
   }
 
-  let mut reader = BufReader::new(io::stdin());
   let split_char = ',';
 
   let mut line = String::new();
