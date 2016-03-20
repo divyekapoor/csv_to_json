30bde79889fae2891a21ff61b5fecbeb132aeb92
Divye Kapoor <divyekapoor@gmail.com>
2016-03-20 15:33:05 -0700

    Minor change to usage and style

--- csv_to_json.rs
+++ csv_to_json.rs
@@ -2,11 +2,12 @@ use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::env;
-use std::process::exit;
+use std::process;
 use std::fs::File;
 
 fn usage() {
   println!("Usage: cat input.csv | csv_to_json");
+  println!("Usage: csv_to_json input.csv");
 }
 
 fn main() {
@@ -17,12 +18,12 @@ fn main() {
 
   if args.len() > 2 {
     usage();
-    exit(1);
+    process::exit(1);
   } else if args.len() == 2 {
     let file = File::open(args[1].clone());
     if !file.is_ok() {
       usage();
-      exit(1);
+      process::exit(1);
     }
     filereader = BufReader::new(file.unwrap());
     reader = &mut filereader;
